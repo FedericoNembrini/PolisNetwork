c2cd1850cbeba25f15e16b6a51c781e461e51962
Federico Nembrini <fredric_99@hotmail.it>
2018-04-09 15:09:59 +0200

    Test Sever Http

--- PolisNetwork.py
+++ PolisNetwork.py
@@ -5,6 +5,7 @@ class MyHttpHandler(http.server.BaseHTTPRequestHandler):
     # GET Command Handler
     def do_GET(self):
         try:
+            print(self.path.encode())
             self.wfile.write(self.path.encode())
         except IOError:
             self.send_error(404, 'file not found')
@@ -12,7 +13,7 @@ class MyHttpHandler(http.server.BaseHTTPRequestHandler):
 def run():
     print('Server is starting...')
 
-    server_address = ('192.168.1.4', 80)
+    server_address = ('192.168.1.4', 8000)
     httpd = http.server.HTTPServer(server_address, MyHttpHandler)
     print('Server is running...')
     httpd.serve_forever()
