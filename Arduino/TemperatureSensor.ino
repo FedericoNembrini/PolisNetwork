1d21367cc856fb3bd24fb6cd994946a8c9d1f503
Federico Nembrini <fredric_99@hotmail.it>
2018-04-21 17:19:12 +0200

    TemperatureSensor now working with the HTTPSever

--- Arduino/TemperatureSensor.ino
+++ Arduino/TemperatureSensor.ino
@@ -1,50 +1,51 @@
-#include <SPI.h>
 #include <Fishino.h>
 #include <SD.h>
+#include <SPI.h>
 #include <FishinoUdp.h>
 #include <ArduinoJson.h>
 
-#define FILE "settings.json"
+#define FILE "SETTIN~1.JSO"
 
 const int chipSelect = 4;
 
+static char *IP_HUB_POLIS_NETWORK;
 static char *SSID, *PASSW, *THINGTAG, *METRICTAG;
-static int IP_HUB_POLIS_NETWORK;
 static int Analog_Input = A7;
 FishinoClient client;
 
 void setup()
 {
-    Serial.begin(250000);
+    Serial.begin(115200);
 
     SPI.begin();
-    SPI.setClockDivider(SPI_CLOCK_DIV4);
+    SPI.setClockDivider(SPI_CLOCK_DIV2);
     
     pinMode(Analog_Input, INPUT);
     
     LoadSettings();
-    delay(1000);
+    delay(2000);
     WifiConnection();
 }
 
 void loop()
 {
-    /*double temperature = LeggiTemperatura();
+    double temperature = LeggiTemperatura();
 
     if(client.connect(IP_HUB_POLIS_NETWORK, 80))
     {
-        Serial.println(temperature);
+        //Serial.println(temperature);
         String request = "GET /?thingTag=";
         request.concat(THINGTAG);
-        request.concat("&thingMetric=");
+        request.concat("&metricTag=");
         request.concat(METRICTAG);
         request.concat("&value=");
-        request.concat(temperature);
-        
-        client.println(request);
+        request.concat(String(temperature));
+        client.print(request);
+        client.flush();
+        client.stop();
     }
 
-    delay(120000);*/
+    delay(120000);
 }
 
 void LoadSettings()
@@ -52,8 +53,8 @@ void LoadSettings()
     Serial.println("Initializing Card...");
 
     while(!SD.begin(chipSelect));
-    
-    Serial.println("Card Initialized.");
+    delay(2000);
+    Serial.println("Card Initialized...");
     
     char json[160];
     File fileSettings = SD.open(FILE);
@@ -68,20 +69,20 @@ void LoadSettings()
     IP_HUB_POLIS_NETWORK = root["ipHub"];
     THINGTAG = root["thingTag"];
     METRICTAG = root["metricTag"];
-
 }
 
 void WifiConnection()
 {
-    while(!Fishino.reset());
-    Serial.println("Fishino Reeboted Correctly");
-        
     Fishino.setMode(STATION_MODE);
     
-    while(!Fishino.begin(SSID, PASSW));
-    Serial.println("Fishino Connected");
+    while(!Fishino.begin(SSID, PASSW))
+    {
+        Serial.println("Ciao");
+    }
+    ;
+    Serial.println("Fishino Connected...");
     
-    delay(8000);
+    delay(10000);
     Serial.println("Fishino IP:");
     Serial.println(Fishino.localIP());
 }
